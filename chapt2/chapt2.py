0e49ef75e5bd3e9744503dd96d35c9867895f556
leonidas <wang_siqiang@163.com>
2018-12-21 16:29:55 +0800

    commit chapter 2

--- chapt2/chapt2.py
+++ chapt2/chapt2.py
@@ -20,21 +20,24 @@ class perceptron():
         self.yita = 0.1
         
         self.readData()
-        self.w0 = np.array([random.random(), random.random()])
+        self.w0 = np.array([random.random() for it in self.data[0]])
         self.b0 = random.random()
 
         self.k = 0
+        self.kk = 0
         
         self.alpha0 = np.array([0 for it in self.data])
         self.beta0 = 0
+        
+        
     def readData(self):
         f = open("data/data.txt")
         r = f.readlines()
         for it in r:
             temp = it.split("\t")
             if it != None:
-                self.data.append(np.array([float(temp[0]), float(temp[1])]))
-                self.label.append(int(temp[2]))
+                self.data.append(np.array([float(x) for x in temp[:-1] ]))
+                self.label.append(int(temp[-1]))
     
     def trainOri(self,yita = 0.1):
         self.w = self.w0
@@ -42,11 +45,38 @@ class perceptron():
         misDivision = True
         self.yita = yita
         self.k = 0
+        self.kk = 0
         while misDivision:
             for it in range(len(self.data)):
+                self.kk +=1
                 if self.label[it] * (np.dot(self.w, self.data[it]) + self.b) <= 0:
                     self.w += self.yita * self.label[it] * self.data[it]
                     self.b += self.yita * self.label[it]
+                    self.k += 1
+                    break
+                if it == len(self.data) - 1:
+                    misDivision = False
+    
+    def trainDualOld(self, yita = 1):
+        self.alpha = self.alpha0
+        self.beta = self.beta0
+        gram = []
+        for it in self.data:
+            temp = []
+            for ot in self.data:
+                temp.append(np.dot(it,ot))
+            gram.append(temp)        
+        misDivision = True
+        self.yita = yita
+        self.k = 0
+        self.kk = 0
+        while misDivision:
+            for it in range(len(self.data)):
+                temp = 0
+                self.kk +=1
+                if self.label[it] * (sum([self.alpha[i] * self.label[i] * gram[i][it] for i in range(len(self.data))]) + self.beta) <= 0:
+                    self.alpha[it] += self.yita
+                    self.beta += self.yita * self.label[it]
                     self.k += 1
                     break
                 if it == len(self.data) - 1:
@@ -60,24 +90,24 @@ class perceptron():
             temp = []
             for ot in self.data:
                 temp.append(np.dot(it,ot))
-            gram.append(temp)        
+            gram.append(temp)
+        gramA = np.array(gram)
         misDivision = True
         self.yita = yita
         self.k = 0
+        self.kk = 0
         while misDivision:
+            ay = np.array([self.alpha[x] * self.label[x] for x in range(len(self.alpha))])
             for it in range(len(self.data)):
                 temp = 0
-#                for i in range(len(self.data)):
-#                    temp += alpha[i] * self.label[i] * gram[it][i]
-#                if self.label[it] * (temp + b) <= 0:
-                if self.label[it] * (sum([self.alpha[i] * self.label[i] * gram[i][it] for i in range(len(self.data))]) + self.beta) <= 0:
+                self.kk +=1
+                if self.label[it] * (np.dot(ay, gramA[it]) + self.beta) <= 0:
                     self.alpha[it] += self.yita
                     self.beta += self.yita * self.label[it]
                     self.k += 1
                     break
                 if it == len(self.data) - 1:
-                    misDivision = False
-        
+                    misDivision = False    
         
     def iterationNumber(self):
         R = 0
@@ -101,11 +131,12 @@ if __name__ == "__main__":
     Perceptron.trainOri(0.3)
     t1 = time.time()
     kMax = Perceptron.iterationNumber()
-    print(Perceptron.w, Perceptron.b, t1 - t0, t0 - t) 
-    print("Iteration for ",Perceptron.k,'times.\nIdeal iteration number is', kMax)
-    print(int(kMax/Perceptron.k))
+    print("Load data cosr",t0 - t,"s\nPerceptronOri cost ", t1 - t0,"s\niterate for",Perceptron.kk, "times, update for",Perceptron.k, "times") 
+#    print(Perceptron.w, Perceptron.b, t1 - t0, t0 - t)
+#    print("Iteration for ",Perceptron.k,'times.\nIdeal iteration number is', kMax)
+#    print(int(kMax/Perceptron.k))
     t2 = time.time()
     Perceptron.trainDual()
-    kMax = Perceptron.iterationNumber()
     t3 = time.time()
-    print(Perceptron.alpha, Perceptron.beta, t3 - t2) 
+    print("PerceptronDual cost ",t3 - t2,"s\niterate for",Perceptron.kk, "times, update for",Perceptron.k, "times") 
+#    print(Perceptron.alpha, Perceptron.beta, t3 - t2) 
--- chapt2/dataGerenation.py
+++ chapt2/dataGerenation.py
@@ -24,5 +24,25 @@ def dataGeneration(length = 1000):
             f.write("\n")
     f.close()
     
+def dataGenerationPro(length = 1000, size = 1000):
+    f = open("data/data.txt","w")
+    for i in range(length):
+        temp = []
+        s = ""
+        for j in range(size):
+            it = random.random() * 100 - 50
+            temp.append(it)
+            s += str(it) + "\t "
+        if sum(temp) >= 0:
+            s += "1"
+            f.write(s)
+        else:
+            s += "-1"
+            f.write(s)
+        if i < length - 1 :
+            f.write("\n")
+    f.close()
+    
 if __name__ == "__main__":
-    dataGeneration()
+#    dataGeneration()
+    dataGenerationPro(1000,1000)
